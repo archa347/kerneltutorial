55f2f5cf1943ef5b2fb2e5806405efca9dbe9998
Daniel Gallegos <daniel.r.gallegos@gmail.com>
2014-03-04 20:38:46 -0800

    Updated makefile
    Add monitor calls to main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
 // main.c -- Defines the C-code kernel entry point, calls initialisation routines.
 // Made for JamesM's tutorials 
 
+#include "monitor.h"
+#include "common.h"
+
 int main(struct multiboot *mboot_ptr)
 {
   // All our initialisation calls will go in here.
+
+  //Testing screen instructions
+  monitor_clear();
+  monitor_write("Hello World");
+
   return 0xDEADBABA;
 }
